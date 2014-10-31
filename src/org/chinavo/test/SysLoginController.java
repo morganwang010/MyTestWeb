dc8658661b2edaf85a7ac0c0d602a728a8af304f
morganwang010 <morganwang010@sina.com>
2014-10-31 13:26:16 +0800

    add bean validation,the first validation example is loginForm.

--- src/org/chinavo/test/SysLoginController.java
+++ src/org/chinavo/test/SysLoginController.java
@@ -2,9 +2,13 @@ package org.chinavo.test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
+import org.chinavo.test.form.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,7 +18,14 @@ public class SysLoginController {
 	@RequestMapping(value = "/hello")
 	public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
 		// 如果已经登录，则跳转到管理首页
-	 
+	 model.addAttribute("loginForm", new LoginForm());
+		return "/login";
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST,value={"/authenticate"})
+	public String regist(@ModelAttribute(value = "loginForm") @Valid LoginForm loginForm, BindingResult result, Model model)
+	{
 		return "/login";
 	}
 }
